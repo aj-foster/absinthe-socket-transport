5ee76b6115649ea65585354d25050e409f61c6b3
morgz <danmorgz@googlemail.com>
2021-09-16 20:16:54 +0200

    bump SwiftPhoenixClient to latest

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AbsintheSocketTransport",
-    platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
+    platforms: [.iOS(.v13), .macOS(.v10_14), .tvOS(.v13), .watchOS(.v5)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["AbsintheSocketTransport"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/davidstump/SwiftPhoenixClient.git", from: "1.3.0"),
+      .package(url: "https://github.com/davidstump/SwiftPhoenixClient.git", from: "2.1.1"),
       .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios.git", .upToNextMinor(from: "0.41.0")),
     ],
     targets: [
